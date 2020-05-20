ea7159eb9a82ae9d978a3ff0447bfa4e1b9cbfe2
Your Name <alidevecioglu12324@gmail.com>
2020-05-20 12:38:10 +0300

    Route Fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.post('/db', function (req, res) {
     res.send('POST request to the DB')
   })
 
-app.get('/api', (req, res) => {
+app.get('/anlikapi', (req, res) => {
     res.json({ guncellenmetarihi: "20.05.2020 09:00", sehir: "Iskele", hava: "HAVA AÇIK", sicaklik: "37.3°C", isbasicaklik: "10.6°C", basinc: "1010.3 hPa", gorus: "10KM", ruzgar: "Kuzeydoğudan (30) 12 kt."})
 }) 
 
