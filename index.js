a16bb601477f12b5b5c9bfd6ae87608a38a91e5f
Your Name <alidevecioglu12324@gmail.com>
2020-05-20 12:34:40 +0300

    Web Yönlendirmeler

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
+app.post('/db', function (req, res) {
+    res.send('POST request to the DB')
+  })
+
+app.get('/api', (req, res) => {
+    res.json({ guncellenmetarihi: "20.05.2020 09:00", sehir: "TEST", hava: "HAVA AÇIK", sicaklik: "37.3°C", isbasicaklik: "10.6°C", basinc: "1010.3 hPa", gorus: "10KM", ruzgar: "Kuzeydoğudan (30) 12 kt."})
+}) 
+
 app.listen(process.env.PORT || 3000,
 	() => console.log("Server is running..."));
