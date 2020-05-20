d5987cc5b9e045ef24a2beecd468f6973d37b98b
Your Name <alidevecioglu12324@gmail.com>
2020-05-20 18:47:36 +0300

    Veri Kontrolü

--- index.js
+++ index.js
@@ -97,69 +97,86 @@ async function main() {
     
     console.log("Veriler Ayrıştırıldı")
 
+if (limasoldata == undefined) { console.log("Limasol Verisi Yüklenemedi")} else {
     db.get('limasol')
     .push({ id: 1, istasyon: 'limasol', tarih: limasoldata.tarih, hava: limasoldata.hava, sicaklik: limasoldata.sicaklik, nem: limasoldata.nem, basinc: limasoldata.basinc, gorus: limasoldata.gorus, ruzgar: limasoldata.ruzgar})
     .write()
-
+}
+if (ercandata == undefined) { console.log("ercan Verisi Yüklenemedi")} else {
     db.get('ercan')
     .push({ id: 1, istasyon: 'ercan', tarih: ercandata.tarih, hava: ercandata.hava, sicaklik: ercandata.sicaklik, nem: ercandata.nem, basinc: ercandata.basinc, gorus: ercandata.gorus, ruzgar: ercandata.ruzgar})
     .write()
-
+}
+if (larnakadata == undefined) { console.log("larnaka Verisi Yüklenemedi")} else {
     db.get('larnaka')
     .push({ id: 1, istasyon: 'larnaka', tarih: larnakadata.tarih, hava: larnakadata.hava, sicaklik: larnakadata.sicaklik, nem: larnakadata.nem, basinc: larnakadata.basinc, gorus: larnakadata.gorus, ruzgar: larnakadata.ruzgar})
     .write()
-
+}
+if (bafdata == undefined) { console.log("baf Verisi Yüklenemedi")} else {    
     db.get('baf')
     .push({ id: 1, istasyon: 'baf', tarih: bafdata.tarih, hava: bafdata.hava, sicaklik: bafdata.sicaklik, nem: bafdata.nem, basinc: bafdata.basinc, gorus: bafdata.gorus, ruzgar: bafdata.ruzgar})
     .write()
-
+}    
+if (iskeledata == undefined) { console.log("iskele Verisi Yüklenemedi")} else {  
     db.get('iskele')
     .push({ id: 1, istasyon: 'iskele', tarih: iskeledata.tarih, hava: iskeledata.hava, sicaklik: iskeledata.sicaklik, nem: iskeledata.nem, basinc: iskeledata.basinc, gorus: iskeledata.gorus, ruzgar: iskeledata.ruzgar})
     .write()
-
+}  
+if (gazimagusadata == undefined) { console.log("gazimagusa Verisi Yüklenemedi")} else {  
     db.get('gazimagusa')
     .push({ id: 1, istasyon: 'gazimagusa', tarih: gazimagusadata.tarih, hava: gazimagusadata.hava, sicaklik: gazimagusadata.sicaklik, nem: gazimagusadata.nem, basinc: gazimagusadata.basinc, gorus: gazimagusadata.gorus, ruzgar: gazimagusadata.ruzgar})
     .write()
-
+}
+if (guzelyurtdata == undefined) { console.log("guzelyurt Verisi Yüklenemedi")} else {  
     db.get('guzelyurt')
     .push({ id: 1, istasyon: 'guzelyurt', tarih: guzelyurtdata.tarih, hava: guzelyurtdata.hava, sicaklik: guzelyurtdata.sicaklik, nem: guzelyurtdata.nem, basinc: guzelyurtdata.basinc, gorus: guzelyurtdata.gorus, ruzgar: guzelyurtdata.ruzgar})
     .write()
-
+}
+if (lefkedata == undefined) { console.log("lefke Verisi Yüklenemedi")} else {  
     db.get('lefke')
     .push({ id: 1, istasyon: 'lefke', tarih: lefkedata.tarih, hava: lefkedata.hava, sicaklik: lefkedata.sicaklik, nem: lefkedata.nem, basinc: lefkedata.basinc, gorus: lefkedata.gorus, ruzgar: lefkedata.ruzgar})
     .write()
-
+}
+if (akdenizdata == undefined) { console.log("akdeniz Verisi Yüklenemedi")} else {  
     db.get('akdeniz')
     .push({ id: 1, istasyon: 'akdeniz', tarih: akdenizdata.tarih, hava: akdenizdata.hava, sicaklik: akdenizdata.sicaklik, nem: akdenizdata.nem, basinc: akdenizdata.basinc, gorus: akdenizdata.gorus, ruzgar: akdenizdata.ruzgar})
     .write()
-
+}   
+if (yesilirmakdata == undefined) { console.log("yesilirmak Verisi Yüklenemedi")} else {  
     db.get('yesilirmak')
     .push({ id: 1, istasyon: 'yesilirmak', tarih: yesilirmakdata.tarih, hava: yesilirmakdata.hava, sicaklik: yesilirmakdata.sicaklik, nem: yesilirmakdata.nem, basinc: yesilirmakdata.basinc, gorus: yesilirmakdata.gorus, ruzgar: yesilirmakdata.ruzgar})
     .write()
-    
+}   
+if (girnedata == undefined) { console.log("girne Verisi Yüklenemedi")} else {  
     db.get('girne')
     .push({ id: 1, istasyon: 'girne', tarih: girnedata.tarih, hava: girnedata.hava, sicaklik: girnedata.sicaklik, nem: girnedata.nem, basinc: girnedata.basinc, gorus: girnedata.gorus, ruzgar: girnedata.ruzgar})
     .write()
-
+}
+if (lefkosadata == undefined) { console.log("lefkosa Verisi Yüklenemedi")} else {  
     db.get('lefkosa')
     .push({ id: 1, istasyon: 'lefkosa', tarih: lefkosadata.tarih, hava: lefkosadata.hava, sicaklik: lefkosadata.sicaklik, nem: lefkosadata.nem, basinc: lefkosadata.basinc, gorus: lefkosadata.gorus, ruzgar: lefkosadata.ruzgar})
     .write()
-
+}
+if (gecitkaledata == undefined) { console.log("gecitkale Verisi Yüklenemedi")} else {  
     db.get('gecitkale')
     .push({ id: 1, istasyon: 'gecitkale', tarih: gecitkaledata.tarih, hava: gecitkaledata.hava, sicaklik: gecitkaledata.sicaklik, nem: gecitkaledata.nem, basinc: gecitkaledata.basinc, gorus: gecitkaledata.gorus, ruzgar: gecitkaledata.ruzgar})
     .write()
-
+}
+if (dipkarpazdata == undefined) { console.log("dipkarpaz Verisi Yüklenemedi")} else {  
     db.get('dipkarpaz')
     .push({ id: 1, istasyon: 'dipkarpaz', tarih: dipkarpazdata.tarih, hava: dipkarpazdata.hava, sicaklik: dipkarpazdata.sicaklik, nem: dipkarpazdata.nem, basinc: dipkarpazdata.basinc, gorus: dipkarpazdata.gorus, ruzgar: dipkarpazdata.ruzgar})
     .write()
-
+}
+if (yenierenkoydata == undefined) { console.log("yenierenkoy Verisi Yüklenemedi")} else {  
     db.get('yenierenkoy')
     .push({ id: 1, istasyon: 'yenierenkoy', tarih: yenierenkoydata.tarih, hava: yenierenkoydata.hava, sicaklik: yenierenkoydata.sicaklik, nem: yenierenkoydata.nem, basinc: yenierenkoydata.basinc, gorus: yenierenkoydata.gorus, ruzgar: yenierenkoydata.ruzgar})
     .write()
-
+}    
+if (alsancakdata == undefined) { console.log("alsancak Verisi Yüklenemedi")} else {  
     db.get('alsancak')
     .push({ id: 1, istasyon: 'alsancak', tarih: alsancakdata.tarih, hava: alsancakdata.hava, sicaklik: alsancakdata.sicaklik, nem: alsancakdata.nem, basinc: alsancakdata.basinc, gorus: alsancakdata.gorus, ruzgar: alsancakdata.ruzgar})
     .write()
+}  
 }
 
 
@@ -189,9 +206,8 @@ app.get('/api/:sehir', (req, res) => {
     const veri = db.get(req.params.sehir)
                 .find({ id: 1 })
                 .value()
-    
-    res.json({ guncellenmetarihi: veri.tarih, sehir: veri.istasyon, hava: veri.hava, sicaklik: veri.sicaklik,  basinc: veri.basinc, nem: veri.nem, gorus: veri.gorus, ruzgar: veri.ruzgar})
-
+    if ( veri == undefined) {console.log(req.params.sehir  + " Sorgusu atıldı ama veritabanında yok diye sorgu reddedildi"); res.send('Bu şehir henüz veritabanımıza ekli değil'); return false } 
+    else {res.json({ guncellenmetarihi: veri.tarih, sehir: veri.istasyon, hava: veri.hava, sicaklik: veri.sicaklik,  basinc: veri.basinc, nem: veri.nem, gorus: veri.gorus, ruzgar: veri.ruzgar})}
 
 
  
